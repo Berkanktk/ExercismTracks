5f83b945081ad3ec736b5e9dfdd96fdb59e494b5
Berkanktk <berkank@live.dk>
2022-04-17 01:37:34 +0200

    Finished Cars Assemble

---
+++ Java/src/CarsAssemble.java
@@ -0,0 +1,31 @@
+public class CarsAssemble {
+
+    public double productionRatePerHour(int speed) {
+        if (speed >= 5 && speed <= 8) {
+            return speed * (221 * 0.90);
+        } else if (speed == 9) {
+            return speed * (221 * 0.8);
+        } else if (speed == 10) {
+            return CarsAssemble.round(speed * (221 * 0.77),1);
+        } else {
+            return speed * 221;
+        }
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        double wPerMinute = productionRatePerHour(speed);
+        return (int) (wPerMinute / 60);
+    }
+
+    private static double round (double value, int precision) {
+        int scale = (int) Math.pow(10, precision);
+        return (double) Math.round(value * scale) / scale;
+    }
+
+    public static void main(String[] args) {
+        CarsAssemble carsAssemble = new CarsAssemble();
+
+        System.out.println(carsAssemble.productionRatePerHour(10));
+        System.out.println(carsAssemble.workingItemsPerMinute(6));
+    }
+}
