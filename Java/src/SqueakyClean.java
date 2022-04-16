149d1418d7200076992f087964c1cb2a96c13d36
Berkanktk <berkank@live.dk>
2022-04-16 23:35:00 +0200

    Finished squeeky clean

---
+++ Java/src/SqueakyClean.java
@@ -0,0 +1,31 @@
+class SqueakyClean {
+    static String clean(String identifier) {
+        var stringBuilder = new StringBuilder();
+        var isAfterDash = false;
+
+        for (var c : identifier.toCharArray()) {
+            if (isGreekLowercase(c)) {
+                continue;
+            } else if (isAfterDash) {
+                stringBuilder.append(Character.toUpperCase(c));
+            } else if (Character.isWhitespace(c)) {
+                stringBuilder.append('_');
+            } else if (Character.isISOControl(c)) {
+                stringBuilder.append("CTRL");
+            } else if (Character.isLetter(c)) {
+                stringBuilder.append(c);
+            }
+            isAfterDash = (c == '-');
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private static boolean isGreekLowercase(char c) {
+        return (c >= 'α' && c <= 'ω');
+    }
+
+    public static void main(String[] args) {
+        System.out.println(clean("Hello    World"));
+    }
+}
